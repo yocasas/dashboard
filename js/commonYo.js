cc32b426f2f81f58fa4e71ddca9686d461777602
brunoartc <brunoartc@yahoo.com>
2021-09-15 15:51:34 -0300

    [FIX] minir changes

---
+++ js/commonYo.js
@@ -0,0 +1,39 @@
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+
+
+
+let checkCurrentUser = function () {
+
+    let myCookie = getCookie("login_session");
+    let clientInfo
+
+    if (myCookie == null) {
+        window.onload(window.location.replace("/home"))
+    } else {
+        console.log(`cliente ja esta logado ${myCookie}`)
+        clientInfo = JSON.parse(myCookie.split(".")[1]);
+    }
+
+    return clientInfo
+
+
+
+
+}
